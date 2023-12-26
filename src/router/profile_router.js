cc72c8d4bd6925eeafc94933810da21915c54a0f
rajvi2909 <rajvikumbhani29@gmail.com>
2023-12-26 20:26:13 +0530

    change:profile

--- src/router/profile_router.js
+++ src/router/profile_router.js
@@ -2,11 +2,12 @@ const {Router}=require("express")
 const {addProfile,getProfile}=require("../controller/profile_controller")
 const { EMPLOYEE_ROLE } = require("../config/string")
 const {verifyUser}=require("../middleware/verify_user")
+const multer = require("../middleware/multer");
+
 
 const router=Router()
 
 router.get("/",verifyUser(EMPLOYEE_ROLE),getProfile)
-router.post("/add",verifyUser(EMPLOYEE_ROLE),addProfile)
-router.post("/update",verifyUser(EMPLOYEE_ROLE),addProfile)
+router.post("/add",verifyUser(EMPLOYEE_ROLE), multer.single("profilePic"),addProfile)
 
 module.exports=router
