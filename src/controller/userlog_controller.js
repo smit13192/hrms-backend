b5d56dcae1f8cee21ede6c38d35b82e455d92112
smit monpara <monparasmit1@gmail.com>
2024-01-15 11:18:40 +0530

    CHANGE:- user log

--- src/controller/userlog_controller.js
+++ src/controller/userlog_controller.js
@@ -6,33 +6,106 @@ const { EMPLOYEE_ROLE } = require("../config/string");
 async function checkIn(req, res, next) {
     try {
         req.body.empId = req.id
+        const currentDate = new Date();
+
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const day = currentDate.getDate();
+
+        const currentDateWithoutTime = new Date(year, month, day);
+        req.body.date = currentDateWithoutTime;
+        req.body.checkIn = currentDate;
+
+        const findCurrentDateUserlog = await UserlogModel.findOne({ date: currentDateWithoutTime, empId: req.id });
+        if (findCurrentDateUserlog) {
+            return res.status(400).json({ success: false, message: "You can't second time add your log" });
+        }
+
         const checkIn = new UserlogModel(req.body)
         await checkIn.save()
         res.status(201).json({ success: true, message: "checkIn log added suggesfully" })
     } catch (e) {
-         next(new ApiError(400, e.message))
+        next(new ApiError(400, e.message))
     }
 }
 
 async function checkOut(req, res, next) {
     try {
-        const id = req.params.id
-        await UserlogModel.findByIdAndUpdate({ _id: id }, { $set: req.body }, { new: true })
+        const id = req.id;
+        const currentDate = new Date();
 
-        const userlog = await UserlogModel.findById({ _id: id });
-        const checkInTime = userlog.checkIn
-        const checkOutTime = userlog.checkOut
-        const durationInMillis = checkOutTime - checkInTime;
-        const hours = Math.floor(durationInMillis / (1000 * 60 * 60));
-        const minutes = Math.floor((durationInMillis % (1000 * 60 * 60)) / (1000 * 60));
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const day = currentDate.getDate();
 
-        req.body.hours=hours
-        req.body.minutes=minutes
-        await UserlogModel.findByIdAndUpdate({ _id: id }, { $set: req.body }, { new: true })
+        const currentDateWithoutTime = new Date(year, month, day);
 
-        res.status(201).json({ success: true, message: "checkOut log added suggesfully" })
+        const userlog = await UserlogModel.findOne({ date: currentDateWithoutTime, empId: id });
+        userlog.checkOut = currentDate;
+        const durationInMillis = userlog.checkOut - userlog.checkIn;
+        userlog.hours = Math.floor(durationInMillis / (1000 * 60 * 60));
+        userlog.minutes = Math.floor((durationInMillis % (1000 * 60 * 60)) / (1000 * 60));
+        await userlog.save();
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                data: {
+                    checkingTime: userlog.checkIn,
+                    checkoutTime: userlog.checkOut,
+                    hours: userlog.hours,
+                    minutes: userlog.minutes,
+                    time: durationInMillis,
+                },
+                message: "checkOut log added suggesfully"
+            });
     } catch (e) {
-         next(new ApiError(400, e.message))
+        next(new ApiError(400, e.message))
+    }
+}
+
+async function getTime(req, res, next) {
+    try {
+        const id = req.id;
+        let time = 0;
+        const currentDate = new Date();
+
+        const year = currentDate.getFullYear();
+        const month = currentDate.getMonth();
+        const day = currentDate.getDate();
+
+        const currentDateWithoutTime = new Date(year, month, day);
+
+        const findUserLog = await UserlogModel.findOne({ date: currentDateWithoutTime, empId: id });
+        if (findUserLog) {
+            if (findUserLog.checkOut == null) {
+                time = Math.floor((currentDate - findUserLog.checkIn) / 1000)
+            } else {
+                return res.
+                    status(200).
+                    json({
+                        success: true,
+                        data: {
+                            time: Math.floor((findUserLog.checkOut - findUserLog.checkIn) / 1000),
+                            checkingTime: findUserLog.checkIn.toISOString(),
+                            currentTime: currentDate.toISOString()
+                        }
+                    });
+            }
+        }
+        res.
+            status(200).
+            json({
+                success: true,
+                data: {
+                    time: time,
+                    checkingTime: findUserLog.checkIn.toISOString(),
+                    currentTime: currentDate.toISOString()
+                }
+            });
+    } catch (e) {
+        next(new ApiError(400, e.message))
     }
 }
 
@@ -50,8 +123,8 @@ async function getUserlog(req, res, next) {
         }
 
     } catch (e) {
-         next(new ApiError(400, e.message))
+        next(new ApiError(400, e.message))
     }
 }
 
-module.exports = { checkIn, checkOut, getUserlog }
+module.exports = { checkIn, checkOut, getTime, getUserlog }
