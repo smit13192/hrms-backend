3725f3daddfb62b45686df14b548ba6ca5b3432b
smit monpara <monparasmit1@gmail.com>
2024-02-23 22:09:25 +0530

    CHANGE:- 401 to 400

--- src/controller/login_controller.js
+++ src/controller/login_controller.js
@@ -16,7 +16,7 @@ async function login(req, res, next) {
                 res.status(200).json({ statusCode: 200, success: true, message: "login succesfully", token: token });
             }
             else {
-                return next(new ApiError(401, "Password is wrong"))
+                return next(new ApiError(400, "Password is wrong"))
             }
         }
         else {
@@ -28,10 +28,10 @@ async function login(req, res, next) {
                     res.status(200).json({ statusCode: 200, success: true, message: "login succesfully", token: token });
                 }
                 else {
-                    return next(new ApiError(401, "Password is wrong"))
+                    return next(new ApiError(400, "Password is wrong"))
                 }
             } else {
-                return next(new ApiError(401, "Email not exist"))
+                return next(new ApiError(400, "Email not exist"))
             }
         }
     } catch (error) {
@@ -50,10 +50,10 @@ async function employeeLogin(req, res, next) {
                 res.status(200).json({ statusCode: 200, success: true, message: "login succesfully", token: token });
             }
             else {
-                return next(new ApiError(401, "Password is wrong"))
+                return next(new ApiError(400, "Password is wrong"))
             }
         } else {
-            return next(new ApiError(401, "Email not exist"))
+            return next(new ApiError(400, "Email not exist"))
         }
     } catch (error) {
         next(new ApiError(400, error.message))
