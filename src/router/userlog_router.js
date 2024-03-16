3323ed3832f9e871d80a5c185c9f4cdace7ff245
smit monpara <monparasmit1@gmail.com>
2024-03-16 22:56:24 +0530

    DONE:- attandeance

--- src/router/userlog_router.js
+++ src/router/userlog_router.js
@@ -1,6 +1,6 @@
 const { Router } = require("express")
-const { startTime, stopTime, reportingTime, getUserLog, totalWorkingHours } = require("../controller/userlog_controller")
-const { EMPLOYEE_ROLE } = require("../config/string")
+const { startTime, stopTime, reportingTime, getUserLog, totalWorkingHours, attendance } = require("../controller/userlog_controller")
+const { EMPLOYEE_ROLE, COMPANY_ROLE } = require("../config/string")
 const { verifyUser } = require("../middleware/verify_user")
 
 const router = Router()
@@ -10,5 +10,7 @@ router.post("/stop-time", verifyUser(EMPLOYEE_ROLE), stopTime)
 router.get("/reporting-time", verifyUser(EMPLOYEE_ROLE), reportingTime)
 router.get("/", verifyUser(EMPLOYEE_ROLE), getUserLog)
 router.get("/total-working-hours", verifyUser(EMPLOYEE_ROLE), totalWorkingHours)
+router.get("/attendance", verifyUser(EMPLOYEE_ROLE), attendance)
+router.get("/attendance/:id", verifyUser(COMPANY_ROLE), attendance)
 
 module.exports = router
